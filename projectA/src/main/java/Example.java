f837c6ab8289f00890dae90d49f05ccf80352cfe
BrianAnsems <brianansems@gmail.com>
2015-05-05 23:23:04 -0400

    typo

--- projectA/src/main/java/Example.java
+++ projectA/src/main/java/Example.java
@@ -1,4 +1,4 @@
-mport org.springframework.boot.*;
+import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
